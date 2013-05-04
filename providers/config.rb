6f660eacf4a5f0c7d52eb7d66977db850a3569f8
hansotronic <serviceshansen@gmail.com>
2013-05-04 16:56:21 +0200

    fix

--- providers/config.rb
+++ providers/config.rb
@@ -20,7 +20,7 @@ action :add_multiline do
       echo -en "#{new_resource.string}\n" >> /etc/ssh/sshd_config
     }
     not_if %{ [[ ! $(cat /etc/ssh/sshd_config) =~ "#{new_resource.match}" ]] }
-    notifies :restart, resources(:service => node[:ssh][:service_name], :delayed
+    notifies :restart, resources(:service => node[:ssh][:service_name]), :delayed
   end
 end
 
@@ -31,6 +31,6 @@ action :remove do
       sed -i '/#{new_resource.match}.*/ d' /etc/ssh/sshd_config
     }
     only_if %{ egrep -c "^#{new_resource.string}$" /etc/ssh/sshd_config -q }
-    notifies :restart, resources(:service => node[:ssh][:service_name], :delayed
+    notifies :restart, resources(:service => node[:ssh][:service_name]), :delayed
   end
 end
